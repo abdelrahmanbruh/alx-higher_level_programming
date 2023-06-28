be4415c3b99bf1b48a4bfe1ce5dade630531e42e
abdelrahmanbruh <abdalrahman.moushref3@gmail.com>
2023-06-28 03:48:41 +0200

    cuh

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class called Square."""
+
+class Square:
+    """make a square class."""
+
+    def __init__(self, size):
+        """Initialize a square
+
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
