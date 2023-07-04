74ef31564a53a78cc3c03983cb5e882ff52af04e
abdelrahmanbruh <abdalrahman.moushref3@gmail.com>
2023-07-04 03:57:04 +0200

    cuh

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
         number_of_instances (int): number of Rectangle instances.
         """
 
-        number_of_instances = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initialize Rectangle.
