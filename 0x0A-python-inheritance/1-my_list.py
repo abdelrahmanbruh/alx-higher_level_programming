7352689076be5fbae8e5383cd9ba1a7ceeb37e16
abdelrahmanbruh <abdalrahman.moushref3@gmail.com>
2023-07-11 00:38:28 +0200

    cuh

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+TODO: make a mylist function
+"""
+
+
+class MyList(list):
+    """class for my list"""
+    pass
+
+    def print_sorted(self):
+        """method to print the sorted list"""
+
+        print(sorted(list(self)))
