9579f803b0a1feb8d6ec6c91a25f8c23f37522aa
abdelrahmanbruh <abdalrahman.moushref3@gmail.com>
2023-07-11 19:36:18 +0200

    cuh

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
+"""define le add"""
 import json
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
 
-my_list = []
 
-for arg in sys.argv[1:]:
-    my_list.append(arg)
-
-save_to_json_file(my_list, 'add_item.json') 
-
-loaded_list = load_from_json_file('add_item.json')
-
-print(my_list)
-print(loaded_list)
+def save_to_json_file(my_obj, filename):
+    """wadshada"""
+    with open(filename, "w", encoding="UTF-8") as f:
+        json.dump(my_obj, f)
