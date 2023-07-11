2877591bf5e59092841e8905a02962bedb942593
abdelrahmanbruh <abdalrahman.moushref3@gmail.com>
2023-07-11 04:57:14 +0200

    cuh

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
+"""Define Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
-"""
-BaseGeometry
-"""
 
 
 class Square(Rectangle):
-    """asdaf"""
+    """Represent a square."""
 
     def __init__(self, size):
-        """Method for attrubutes"""
+        """Initialize new square.
 
-        super().__init__(size, size)
+        Args:
+            size (int): size of the new square.
+        """
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        """rectangle area"""
-
-        return self.__size ** 2
-
-    def __str__(self):
-        return "[Square] {}/{}".format(self.__size, self.__size)
